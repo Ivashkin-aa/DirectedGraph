a1f6f430fff748499600019cce26419cdb173b8a
ivashkin-aa <ivashkin.aa@edu.spbstu.ru>
2020-03-12 23:49:49 +0300

    Теперь модель данных оптимальнее; Убрал консольной вывод; Есть ошибка, в иницализации методов Rename и Delete, попробую решить через stream.

--- src/main/java/sbstu/Graph.java
+++ src/main/java/sbstu/Graph.java
@@ -1,91 +1,69 @@
 package sbstu;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Graph {
-    private Map<Vertex, Set<Vertex>> neighbors = new HashMap<Vertex, Set<Vertex>>();
+    private Set<Vertex> vertices = new HashSet<>();
     private Map<Pair<Vertex, Vertex>, Integer> edge = new HashMap<Pair<Vertex, Vertex>, Integer>();
 
     public boolean hasVertex(String vertexName) {
         Vertex ver = new Vertex(vertexName);
-        return neighbors.get(ver) == null;
+        return vertices.contains(ver);
     }
 
     public boolean addVertex(String vertexName) {
-        if (hasVertex(vertexName)) {
-            neighbors.put(new Vertex(vertexName), new HashSet<>());
-            System.out.println("Вершина " + vertexName + " успешно создана");
+        if (!hasVertex(vertexName)) {
+            vertices.add(new Vertex(vertexName));
             return true;
-        } else {
-            System.out.println("Имя вершины " + vertexName + " занято");
-            return false;
-        }
+        } else return false;
     }
 
     public boolean deleteVertex(String vertexName) {
         Vertex ver = new Vertex(vertexName);
-        if (!hasVertex(vertexName)) {
-            neighbors.values().stream().map(e -> e.remove(ver)).collect(Collectors.toSet());
-            neighbors.remove(ver);
-            System.out.println("Вершина " + vertexName + " успешно удалена");
+        if (hasVertex(vertexName)) {
+            vertices.remove(ver);
+            //edge.keySet().stream().map(e->e.);
+            for (Pair<Vertex, Vertex> pr : edge.keySet()) {
+                Vertex start = pr.getStart();
+                Vertex end = pr.getEnd();
+                if (start == ver)
+                    edge.remove(pr);
+                if (end == ver);
+                    edge.remove(pr);
+            }
             return true;
-        } else {
-            System.out.println("Вершина с именем " + vertexName + " не найдена");
-            return false;
-        }
+        } else return false;
     }
 
     public boolean addEdge(String start, String end, int weight) {
         Vertex ver1 = new Vertex(start);
         Vertex ver2 = new Vertex(end);
-        if (neighbors.get(ver1) != null && neighbors.get(ver2) != null) {
-            neighbors.get(ver1).add(ver2);
-            Pair ed = new Pair(ver1, ver2);
+        if (hasVertex(start) && hasVertex(end)) {
+            Pair<Vertex, Vertex> ed = new Pair<>(ver1, ver2);
             edge.put(ed, weight);
-            System.out.println("Дуга " + start + " - " + end + " успешно создана");
             return true;
-        } else {
-            System.out.println("Имена вершин не найдены");
-            return false;
-        }
+        } else return false;
     }
 
     public boolean deleteEdge(String start, String end) {
         Vertex ver1 = new Vertex(start);
         Vertex ver2 = new Vertex(end);
-        Pair verEd;
-        Set<Vertex> ed = neighbors.get(ver1);
-        if (ed != null) {
-            ed.remove(ver2);
-            verEd = new Pair(ver1, ver2);
+        Pair<Vertex, Vertex> verEd = new Pair<>(ver1, ver2);
+        if (edge.get(verEd) != null) {
             edge.remove(verEd);
-            System.out.println("Дуга " + start + " - " + end + " успешно удалена");
             return true;
-        } else {
-            System.out.println("Дуга " + start + " - " + end + " не найдена");
-            return false;
-        }
+        } else return false;
     }
 
     public boolean renameVertex(String vertexName1, String vertexName2) {
         Vertex ver1 = new Vertex(vertexName1);
         Vertex ver2 = new Vertex(vertexName2);
-        if (hasVertex(vertexName1)) {
-            System.out.println("Вершина с именем "+ vertexName1 + " не найдена");
+        if (!hasVertex(vertexName1)) {
             return false;
-        }
-        else {
-            Set<Vertex> v = neighbors.get(ver1);
-            neighbors.remove(ver1);
-            neighbors.put(ver2, v);
-            for (Set<Vertex> val : neighbors.values()) {
-                if (val.contains(ver1)) {
-                    val.remove(ver1);
-                    val.add(ver2);
-                }
-            }
-            for (Pair key : edge.keySet()) {
+        } else {
+            vertices.remove(ver1);
+            vertices.add(ver2);
+            for (Pair<Vertex, Vertex> key : edge.keySet()) {
                 if (key.getStart() == ver1) {
                     key.setStart(ver2);
                 }
@@ -93,7 +71,6 @@ public class Graph {
                     key.setEnd(ver2);
                 }
             }
-            System.out.println("Новое имя вершины \"" + vertexName1 + "\" - \"" + vertexName2 + "\"");
             return true;
         }
     }
@@ -101,33 +78,34 @@ public class Graph {
     public boolean changeWeight(String start, String end, int newWeight) {
         Vertex ver1 = new Vertex(start);
         Vertex ver2 = new Vertex(end);
-        Pair verEd = new Pair(ver1, ver2);
+        Pair<Vertex, Vertex> verEd = new Pair<>(ver1, ver2);
         if (edge.get(verEd) != null) {
             edge.put(verEd, newWeight);
-            System.out.println("Новый вес дуги \"" + start + "-" + end + "\" = " + newWeight);
             return true;
-        } else {
-            System.out.println("Дуга не найдена");
-            return false;
-        }
+        } else return false;
     }
 
     public Set<Vertex> outgoing(String vertexName) {
-        if (!hasVertex(vertexName)) {
-            Set<Vertex> vertices = neighbors.get(new Vertex(vertexName));
-            return vertices;
+        Vertex vr = new Vertex(vertexName);
+        if (hasVertex(vertexName)) {
+            Set<Vertex> out = new HashSet<>();
+            for (Pair<Vertex, Vertex> key : edge.keySet()) {
+                if (key.getStart().equals(vr))
+                    out.add(key.getEnd());
+            }
+            return out;
         } else throw new IllegalArgumentException();
     }
 
     public Set<Vertex> incoming(String vertexName) {
         Vertex ver = new Vertex(vertexName);
         Set<Vertex> in = new HashSet<>();
-        if (!hasVertex(vertexName)) {
-            for (Vertex key : neighbors.keySet())
-                if (neighbors.get(key).contains(ver))
-                    in.add(key);
+        if (hasVertex(vertexName)) {
+            for (Pair<Vertex, Vertex> key : edge.keySet()) {
+                if (key.getEnd().equals(ver))
+                    in.add(key.getStart());
+            }
             return in;
         } else throw new IllegalArgumentException();
     }
-
 }
